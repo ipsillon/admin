4b84c094855d7b942d8869ad8c7b57ffcd51610a
Fernando Fagonde <fernandofagonde@gmail.com>
2022-05-16 23:48:17 -0300

    ajustes

--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Sua senha foi regerada!',
+    'sent' => 'O link para regerar sua senha foi enviado por e-mail!',
+    'throttled' => 'Por favor, aguarde antes de tentar novamente.',
+    'token' => 'Token Inválido.',
+    'user' => "Usuário inválido.",
 
 ];
